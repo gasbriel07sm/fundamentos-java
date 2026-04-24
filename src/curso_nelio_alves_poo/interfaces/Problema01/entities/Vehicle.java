ce092a1abdd0d883081c1b7aa3935c2dbc8bb63a
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-04-24 14:45:00 -0300

    Interfaces Problema 01 - Curso Nélio Alves POO em Java

---
+++ src/curso_nelio_alves_poo/interfaces/Problema01/entities/Vehicle.java
@@ -0,0 +1,18 @@
+package curso_nelio_alves_poo.interfaces.Problema01.entities;
+
+public class Vehicle {
+
+    private String model;
+
+    public Vehicle(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
---
+++ src/curso_nelio_alves_poo/interfaces/Problema01/services/TaxService.java
@@ -0,0 +1,6 @@
+package curso_nelio_alves_poo.interfaces.Problema01.services;
+
+public interface TaxService {
+
+    double tax(double amount);
+}
