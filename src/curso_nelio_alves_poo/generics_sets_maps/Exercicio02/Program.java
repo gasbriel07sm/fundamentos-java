1dfc9c78511c708239b4f6289da8702f26be8fce
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-04-27 16:46:51 -0300

    Refactor Exercício 02 de Set - como testa a igualdade - Curso Nélio Alves POO em Java

--- src/curso_nelio_alves_poo/generics_sets_maps/Exercicio02/Program.java
+++ src/curso_nelio_alves_poo/generics_sets_maps/Exercicio02/Program.java
@@ -8,33 +8,37 @@ public class Program {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        Set<Course> listSet = new HashSet<>();
+        Set<Integer> a = new HashSet<>();
+        Set<Integer> b = new HashSet<>();
+        Set<Integer> c = new HashSet<>();
 
         System.out.print("How many students for course A? ");
-        int courseA = sc.nextInt();
-
-        for (int i = 1; i <= courseA; i++) {
-            int cod = sc.nextInt();
-            listSet.add(new Course(new Student(cod)));
+        int n = sc.nextInt();
+        for (int i=0; i<n; i++) {
+            int number = sc.nextInt();
+            a.add(number);
         }
 
         System.out.print("How many students for course B? ");
-        int courseB = sc.nextInt();
-
-        for (int i = 1; i <= courseB; i++) {
-            int cod = sc.nextInt();
-            listSet.add(new Course(new Student(cod)));
+        n = sc.nextInt();
+        for (int i=0; i<n; i++) {
+            int number = sc.nextInt();
+            b.add(number);
         }
 
         System.out.print("How many students for course C? ");
-        int courseC = sc.nextInt();
-
-        for (int i = 1; i <= courseC; i++) {
-            int cod = sc.nextInt();
-            listSet.add(new Course(new Student(cod)));
+        n = sc.nextInt();
+        for (int i=0; i<n; i++) {
+            int number = sc.nextInt();
+            c.add(number);
         }
 
-        System.out.println("Total students: " + listSet.size());
+        Set<Integer> total = new HashSet<>(a);
+        total.addAll(b);
+        total.addAll(c);
 
+        System.out.println("Total students: " + total.size());
+
+        sc.close();
     }
 }
